d036453786aebf61b3ece7998d5fa83d5c289a11
Ken <keniwane@gmail.com>
2023-12-13 15:34:49 -0800

    react app setup

--- dist/bundle899d5dd23bb83726b320.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-console.log('asdf');
-/******/ })()
-;
-//# sourceMappingURL=bundle899d5dd23bb83726b320.js.map
