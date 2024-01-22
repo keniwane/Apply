9f1c0a6cf86e813305b7a35e5704706833252812
Ken <keniwane@gmail.com>
2024-01-22 13:02:02 -0800

    layout

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Welcome from './Welcome.jsx';
+import JobCard from './JobCard.jsx';
+import Header from './Header.jsx;';
 
 const Dashboard = () => {
   return (
     <div>
+      <Header />
       <Welcome />
+      <JobCard />
     </div>
   );
 };
---
+++ client/src/components/JobCard.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+const JobCard = () => {
+  const [card, setCard] = useState([]);
+
+  return <div className='box-border h-48 w-96 p-4 border-4'>JobCard</div>;
+};
+
+export default JobCard;
