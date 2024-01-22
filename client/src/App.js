baa6626ca7f71b52529328b931139dca1f748300
Ken <keniwane@gmail.com>
2024-01-22 12:44:24 -0800

    initial setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import Dashboard from './components/Dashboard.jsx';
 
 const App = () => {
   return (
     <div>
-      <Dashboard />
+      <nav>
+        {/* Add your navigation links here */}
+        <Link to='/'>Home</Link>
+        {/* Add other links as needed */}
+      </nav>
+
+      {/* Define your routes here */}
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        {/* Add other Route elements for different paths */}
+      </Routes>
     </div>
   );
 };
